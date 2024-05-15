3183c4d2032cf42092df391d4706836c3261f8b3
AJ Johnson <aj132@icloud.com>
2024-05-14 20:45:24 -0400

    Major Update. unfinished
    Transitioning to working branch.

---
+++ src/sections/Skills/SkillDetails.tsx
@@ -0,0 +1,43 @@
+// New Component: SkillDetails.tsx
+import React from 'react';
+import SwiftDetails from './Skill Details/SwiftDetails';
+import PythonDetails from './Skill Details/PythonDetails';
+import DockerDetails from './Skill Details/DockerDetails';
+import GolangDetails from './Skill Details/GolangDetails';
+import ReactDetails from './Skill Details/ReactDetails';
+
+interface SkillDetailsProps {
+    selectedSkill: string;
+}
+
+const SkillDetails: React.FC<SkillDetailsProps> = ({ selectedSkill }) => {
+    let SkillComponent;
+    switch (selectedSkill.slice(0, -1)) {
+        case 'Swift':
+            SkillComponent = SwiftDetails;
+            break;
+        case 'Python':
+            SkillComponent = PythonDetails;
+            break;
+        case 'Docker':
+            SkillComponent = DockerDetails;
+            break;
+        case 'Golang':
+            SkillComponent = GolangDetails;
+            break;
+        case 'React':
+            SkillComponent = ReactDetails;
+            break;
+        default:
+            SkillComponent = () => <p>Selected skill details not available.</p>;
+    }
+
+    return (
+        <div className="skill-details">
+            <h2>{selectedSkill.slice(0, -1)}</h2>
+            <SkillComponent />
+        </div>
+    );
+};
+
+export default SkillDetails;
